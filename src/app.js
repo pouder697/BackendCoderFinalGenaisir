c6aa0a2079d2ab21c9be3e5242b58563fd1a4e56
Andres Genaisir <andres.genaisir@gmail.com>
2024-11-03 12:50:44 -0300

    cambios en la estructura del proyecto

--- src/app.js
+++ src/app.js
@@ -1,70 +1,18 @@
-class ProductManager {
-  constructor() {
-    this.products = [];
-  }
+const express = require("express");
+const productRouter = require("./routes/products.router.js");
+const cartsRouter = require("./routes/carts.router.js");
+const app = express();
+const PORT = 8080;
 
-  static id = 0;
+//Middleware para trabajar con JSON
+app.use(express.json());
 
-  addProducts(title, description, price, thumbnail, code, stock) {
-    for (let i = 0; this.products.length; i++) { //recorremos los productos de segun el largo del array
-      if (this.products[i].code === code) {
-        //si los codigos que conseguimos recorriendo el array son iguales al codigo que recibimmos en la funcion
-        console.log(`El codigo ${code} ya esta en uso`); // devolvemos que el codigo ya esta en uso
-        break;
-      }
-    }
-      const newProduct = {
-        title,
-        description,
-        price,
-        thumbnail,
-        code,
-        stock,
-      };
 
-      if(!Object.values(newProduct).includes(undefinded)){
-      ProductManager.id++;
-      this.products.push({
-        ...newProduct,
-        id: ProductManager.id,
-      });
-     }else{
-      console.log("Todos los campos son requeridos!")
-     }
-  }
+//Routes
+app.use("/api/products", productRouter);
+app.use("/api/carts", cartsRouter);
 
-  getProducts() {
-    return this.products;
-  }
 
-  alreadyExist(id) {
-    return this.products.find((producto) => producto.id === id);
-  }
-
-  getProductById(id) {
-    this.alreadyExist(id)
-      ? console.log(this.alreadyExist(id))
-      : console.log("Producto no encontrado");
-  }
-}
-
-const productos = new ProductManager();
-
-//primer llamado de metodo getProducts que devuelve el array vacio de nuestra instancia
-console.log(productos.getProducts());
-
-//agregamos productos a nuestro objeto
-productos.addProducts("Andres", "Persona de 27 anios", 45, "imagen", "a1", 10);
-productos.addProducts(
-  "Roberto",
-  "Persona de 57 anios",
-  50,
-  "imagen2",
-  "a2",
-  20
-);
-
-//volvemos a imprimir por consola el llamado de getProducts el cual esta vez nos devuelve
-console.log(productos.getProducts());
-
-productos.getProductById(1);
+app.listen(PORT, () => {
+    console.log(`Listening in http://localhost:${PORT}`);
+})
